6fd033e540ac3fbe186a455070140e9023d16fa6
jaegyu <jpmlyr@gmail.com>
2017-01-17 18:55:05 +0900

    .

---
+++ javascriptEx/p4.js
@@ -0,0 +1,20 @@
+/**
+ * Created by jaegyuhan on 2017. 1. 17..
+ */
+
+console.log("hh");
+
+var myObject = new Object();
+
+myObject[0] = 'f';
+myObject[1] = 'o';
+myObject[2] = 'o';
+
+console.log(myObject);
+
+var myString = new String('foo');
+
+console.log(myString);
+
+
+
